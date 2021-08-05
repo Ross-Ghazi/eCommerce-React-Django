a68b539b445d43a3643e4dad5db726fdd52e88bc
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-05 08:41:58 -0600

    12 to minute 14:28

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .products import products
 # Create your views here.
+
+def getRoutes(request):
+    routes=["wwwww","eeeee"]
+    return JsonResponse(routes, safe=False)
+
+def getProducts(request):
+   
+    return JsonResponse(products, safe=False)
