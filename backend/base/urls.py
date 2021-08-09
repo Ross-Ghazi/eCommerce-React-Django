5195ca63ecee2c0772ced9d4b8ffa958c39afee3
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-09 13:18:09 -0600

    end of 33

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
     path('',views.getRoutes),
     path('products/',views.getProducts),
     path('products/<str:pk>',views.getProduct),
-    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-]
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    ]
