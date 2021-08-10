8a2a9fe9ce591d22813e896fc6141251eb7dc773
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-09 18:56:32 -0600

    34  13:17

--- backend/base/Serializers.py
+++ backend/base/Serializers.py
@@ -3,10 +3,23 @@ from django.contrib.auth.models import User
 from .models import Product
 
 class USerSerializer(serializers.ModelSerializer):
+    name=serializers.SerializerMethodField(read_only=True)
+    _id=serializers.SerializerMethodField(read_only=True)
+    isAdmin=serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = User
-        fields = ["id","username", "email"]
+        fields = ["id","_id","username", "email", "name","isAdmin"]
+    def get_name(self,obj):
+        name=obj.first_name
+        if name=="":
+            name=obj.email
+        return name
 
+    def get__id(self,obj):
+        return obj.id
+
+    def get_isAdmin(self,obj):
+        return obj.is_staff
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
