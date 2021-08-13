cb59ac751e74af0f818f1bb9fd4076b3f077d89c
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-13 10:30:10 -0600

    end of 43

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,6 +4,7 @@ from base.views import user_views as views
 
 urlpatterns = [
     path('profile/',views.getUserProfile),
+    path('profile/update/',views.updateUserProfile),
     path('',views.getUsers),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/',views.registerUsers ),
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -31,6 +31,8 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
+
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):   
@@ -65,4 +67,24 @@ def registerUsers(request):
         message={"detail: User already exists"}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):   
+    user=request.user
+    serializer=USerSerializerWithToken(user, many=False)
+    data=request.data
+    user.first_name=data["name"]
+    user.username=data["email"]
+    user.email=data["email"]
+
+
+    if  data["password"] !="":
+        user.password=make_password(data["password"])
+
+    user.save()
+
+    return Response(serializer.data)
 
