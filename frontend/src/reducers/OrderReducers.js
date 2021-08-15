232d756ddde3ad2b37a35b21c303767d63d6e095
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-15 16:13:42 -0600

    end of 52 3:00

---
+++ frontend/src/reducers/OrderReducers.js
@@ -0,0 +1,12 @@
+import {
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL,
+} from "../constants/orderConstants";
+
+export const OrderCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
