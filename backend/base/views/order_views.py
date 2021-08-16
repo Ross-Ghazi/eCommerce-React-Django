f9b667c767924dc2909f5531edcd2093cf56c7d5
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-16 10:20:42 -0600

    end of 54

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -71,7 +71,7 @@ def getOrderById(request,pk):
         order=Order.objects.get(_id=pk)
         if user.is_staff or order.user==user:
             serializer=OrderSerializer(order, many=False)
-            return Response(serializer)
+            return Response(serializer.data)
         else:
             return Response ({"detail":" Not authorized to view this order"}, status=status.HTTP_400_BAD_REQUEST)
     except:
