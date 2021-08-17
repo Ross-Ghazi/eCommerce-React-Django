332ccc64ba34d04041a63d1720cf4908b04a2a0b
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-16 19:24:12 -0600

    end of 56

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -4,5 +4,6 @@ from base.views import order_views as views
 
 urlpatterns = [
    path("add/", views.addOrrderItems),
-   path("<str:pk>/", views.getOrderById)
+   path("<str:pk>/", views.getOrderById),
+   path("<str:pk>/pay", views.updateOrderToPaid)
     ]
