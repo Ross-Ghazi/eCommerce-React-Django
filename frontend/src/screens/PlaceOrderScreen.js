aaf3f272207753e22b650dde693e6e30dd40a6e1
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-09-02 09:42:40 -0600

    fixed update profile message

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -33,7 +33,7 @@ function PlaceOrderScreen({ history }) {
       history.push(`/order/${order._id}`);
       dispatch({ type: ORDER_CREATE_RESET });
     }
-  }, [success, history, dispatch, order._id]);
+  }, [success, history, dispatch]);
 
   const placeOrder = () => {
     dispatch(
