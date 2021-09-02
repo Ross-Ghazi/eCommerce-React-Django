d0d0864bdce969b04658fb43413d68e995f351a6
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-09-01 22:47:51 -0600

    fixed production bugs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn backend.wsgi --log-fil -
+web: gunicorn backend.wsgi --log-file -
--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','ecommerce-django-react-project.herokuapp.com', 'www.onlineshopsample.ca', 'onlineshopsample.ca']
 
 # Application definition
 
